b929a5cf1c2e904d60bc73c0195d3dd4730b32bd
rakib ahmed <rakibahmed.dev@gmail.com>
2022-03-25 15:55:22 +0600

    all product styled done with css

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,11 +6,29 @@ const Product = (props) => {
   const { img, name, price, sellers, rattings } = props.product;
   return (
     <div className="product">
-      <img src={img} alt="" />
-      <h3>{name}</h3>
-      <h5>Price: {price}</h5>
-      <span>Manufacturer: {sellers}</span>
-      <span>Ratting: {rattings}</span>
+      <div className="product-img">
+        <img src={img} alt="" />
+      </div>
+      <div className="product-detail">
+        <h3>{name}</h3>
+        <h5>Price: ${price}</h5>
+        <span>Manufacturer: {sellers}</span>
+        <span>Ratting: {rattings}</span>
+      </div>
+      <button className="btn-cart">
+        Add To Cart{" "}
+        <span>
+          <svg
+            className="cart-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+          </svg>
+        </span>
+      </button>
     </div>
   );
 };
